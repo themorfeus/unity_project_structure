1ddc818ca52ddfa7c2dcf0bc5774eff5afec2412
themorfeus <TheMorfeus@users.noreply.github.com>
2019-02-21 20:28:50 +0100

    Initial commit

---
+++ Assets/_project_name/Scripts/Misc/Editor/TorusControllerInspector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TorusController))]
+public class TorusControllerInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        TorusController targetTorus = (TorusController)target;
+
+        targetTorus.SetSpinSpeed(EditorGUILayout.Slider("Spin Speed", targetTorus.GetSpinSpeed(), -10, 10));
+    }
+}
---
+++ Assets/_project_name/Scripts/Misc/TorusController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This controls the torus spinning
+public class TorusController : MonoBehaviour
+{
+
+    [SerializeField] private float spinSpeed;
+
+    private void Update()
+    {
+        transform.Rotate(spinSpeed * Time.deltaTime, 0, -spinSpeed * Time.deltaTime);
+    }
+
+    public float GetSpinSpeed()
+    {
+        return spinSpeed;
+    }
+
+    public void SetSpinSpeed(float spinSpeed)
+    {
+        this.spinSpeed = spinSpeed;
+    }
+}
